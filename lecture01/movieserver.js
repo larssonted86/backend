6c559b5003abb87f690aacd20fb8e1a8aa46ac20
Ted Larsson <ted@pop-os.localdomain>
2020-05-03 11:43:07 +0200

    backup

--- lecture01/movieserver.js
+++ lecture01/movieserver.js
@@ -2,14 +2,22 @@
 const express = require(`express`);
 const app = express();
 const fs = require(`fs`);
-const DB_PATH = `movies.json`
+const DB_PATH = `./public/movies.json`
 let movies = [];
 let id = 1;
 
 
 //FUNCTIONS
 save = () =>{
-    fs.writeFile
+  return new Promise((resolve,reject) =>{
+    fs.writeFile(DB_PATH, JSON.stringify(movies), (err)=>{
+      if(err){
+        reject(err);
+      }else{
+        resolve();
+      }
+    });
+  });  
 }
 
 //MIDDLEWARES
@@ -18,71 +26,72 @@ app.use(express.static(`public`));
 
 //SERVER BUILD
 app.get(`/movies`, (req,res)=>{
-    res.json({
-        data: movies
-    })
+  res.json({
+      data: movies
+  })
 })
 app.get(`/movies/:id`, (req,res)=>{
-    let id = parseInt(req.params.id);
-    let movie = movies.find(function(movie){
-       return movie.id === id;
-   })
+  let id = parseInt(req.params.id);
+  let movie = movies.find(function(movie){
+      return movie.id === id;
+  })
 
-   if(movie){
-       res.status(200).json(movie)
-   }else {
-       res.status(400).end()
-   }
+  if(movie){
+      res.status(200).json(movie)
+  }else {
+      res.status(400).end()
+  }
 })
 
 app.post(`/movies`, (req,res)=>{
-    let movie = req.body;
-    if(!movie.title){
-        res.status(400).end()
-        return
-    }
-        movie.id = id;
-        movies.push(movie)
-        id += 1;
-        res.status(201).send(movie);    
-    res.end();
+  let movie = req.body;
+  if(!movie.title){
+      res.status(400).end()
+      return
+  }
+      movie.id = id;
+      movies.push(movie)
+      id += 1;
+      save();
+      res.status(201).send(movie);    
+  res.end();
 })
 
 app.put(`/movies/:id`, (req,res) =>{
-    let movie = req.body;
-    let id = parseInt(req.params.id);
-    if(!movie.title || movie.id !== id){
-        res.status(400).end()
-        return
-    }
+  let movie = req.body;
+  let id = parseInt(req.params.id);
+  if(!movie.title || movie.id !== id){
+      res.status(400).end()
+      return
+  }
 
-   let movieIndex = movies.findIndex(function(movie){
-       return movie.id === id;
-   })
-   if(movie === -1){
-       res.status(404).end();
-       return
-   }
+  let movieIndex = movies.findIndex(function(movie){
+      return movie.id === id;
+  })
+  if(movie === -1){
+      res.status(404).end();
+      return
+  }
 
-   movies[movieIndex] = movie
-   res.status(200).send(movie)
+  movies[movieIndex] = movie
+  res.status(200).send(movie)
 })
 
 app.delete(`/movies/:id`, (req,res) =>{
-    let movie = req.body;
-    let id = parseInt(req.params.id);
+  let movie = req.body;
+  let id = parseInt(req.params.id);
 
-    let movieIndex = movies.findIndex(function(movie){
-        return movie.id === id;
-    })
+  let movieIndex = movies.findIndex(function(movie){
+      return movie.id === id;
+  })
 
-    if(movie === -1){
-        res.status(404).end();
-        return
-    }
+  if(movie === -1){
+      res.status(404).end();
+      return
+  }
 
-    movies.splice(movieIndex, 1)
-    res.status(204).end()
+  movies.splice(movieIndex, 1)
+  res.status(204).end()
  
 })
-app.listen(8080, console.log(`server started at 3000`));
+app.listen(3000, console.log(`server started at 3000`));
